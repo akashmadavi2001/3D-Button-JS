d9bcca5c25e90d949b847265a4e1d76c39fccdfb
Akash Madavi <akashmadavi2001@gmail.com>
2025-03-08 08:12:39 +0530

    Add files via upload
    Active Button and Other Unactive Button

---
+++ script.js
@@ -0,0 +1,16 @@
+let boxs = document.querySelectorAll('.box');
+
+boxs.forEach(box => {
+    box.addEventListener("click", () => {
+        let active = document.querySelector('.active');
+        // method 1
+        // active?.classList.remove('active');
+
+        //method 2
+        if (active) {
+            active.classList.remove('active');
+        };
+
+        box.classList.add('active');
+    });
+});
